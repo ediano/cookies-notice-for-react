1784de4509fd2a6ef8a09bb5d10aa7bca30b679a
Ediano Silva Santos <edianodev@gmail.com>
2020-10-08 11:59:37 -0400

    Add componente cookies

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Cookies from './components/Cookies'
+
+import './global.css'
+
+const App: React.FC = () => (
+  <>
+    <h1>Aviso de cookies para ReactJS com TypeScript</h1>
+
+    <Cookies />
+  </>
+);
 
 export default App;
